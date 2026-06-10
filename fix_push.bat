1f9d9911608ff195ec3adc2a1d2be53a3864db37
Nexykon <nexykon@github.com>
2026-06-10 16:34:10 +0200

    update

--- fix_push.bat
+++ fix_push.bat
@@ -1,8 +1,15 @@
 @echo off
 cd /d "%~dp0"
-if exist ".git\index.lock" del /f ".git\index.lock"
+echo Brisem git lock...
+if exist ".git\index.lock" (
+    del /f /q ".git\index.lock"
+    echo Lock odstranjen.
+)
+if exist ".git\COMMIT_EDITMSG.lock" del /f /q ".git\COMMIT_EDITMSG.lock"
+echo Commitam spremembe...
 git add .
-git commit -m "update"
+git commit -m "fix: delivery redirect + logo sizes"
+echo Pushanje na Railway...
 git push
 echo.
 echo DONE! Railway redeploya v ~1 min
